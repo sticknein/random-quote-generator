df73404d5dfce6ec0dbb229f8c8aff4b8a1dbc96
steinFolio <stein1173@yahoo.com>
2021-03-13 08:38:19 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+const quotes = require('./quotes.json');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    const quote = this.randomQuote()
+    this.state = {
+      quote: quote.quote,
+      author: quote.author
+    }
+    this.randomQuote = this.randomQuote.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  randomQuote() {
+    const quote = quotes[Math.floor(Math.random() * quotes.length)]
+    return {
+      quote: quote.Quote,
+      author: quote.Author
+    }
+  }
+
+  handleClick() {
+    const quote = this.randomQuote();
+    this.setState({
+      quote: quote.quote,
+      author: quote.author
+    });
+  }
+
+  render() {
+    return (
+      <body className='body'>
+        <div id='quote-box' className='quote-box'>
+          <p id='text'>"{this.state.quote}"</p>
+          <p id='author'>-{this.state.author}</p>
+          <button id='new-quote' onClick={this.handleClick}>New Quote</button>
+          <a id='tweet-quote' href='https://www.twitter.com/intent/tweet' target='_blank'>Tweet what must be twote</a>
+        </div>
+      </body>
+    )
+  }
 }
 
 export default App;
